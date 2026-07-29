667ce30b5bf83e1ab35b4110c83dfbeba3367e19
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-29 11:50:59 -0300

    diciona parser CLI para consultar atividade do GitHub

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-print("oi")
-print("amnaha")
+import argparse
+
+
+def build_parser() -> argparse.ArgumentParser:
+    """Configura o parser de argumentos CLI."""
+    parser = argparse.ArgumentParser(
+        prog="GiThub User Activity",
+        description="Atividade do usuário do GitHub via linha de comando (CLI)",
+    )
+
+    subparsers = parser.add_subparsers(
+        dest="command", required=True, help="Comandos disponíveis"
+    )
+
+    # Comando: github-activity
+    activity_parser = subparsers.add_parser(
+        "github-activity", help="Retorna a atividade do usuário do GITHUB"
+    )
+    activity_parser.add_argument("username", type=str, help="Nome do usuário do GITHUB")
+
+    return parser
+
+
+def main() -> None:
+    parser = build_parser()
+    args = parser.parse_args()
+
+    if args.command == "github-activity":
+        username = args.username.strip()
+        if not username:
+            print("ERRO: O nome de usuário não pode está vazio.")
+            return
+
+        print(f"Buscando atividades do usuário: {username}")
+
+
+if __name__ == "__main__":
+    main()
